be95abd9583a4b06dae9dc36689e0b61b695ed99
Vlad Golubev <golubev.vld@gmail.com>
2016-03-27 22:48:48 +0300

    Implement task requirement

--- index.js
+++ index.js
@@ -2,7 +2,23 @@
 
 'use strict';
 
-require('shelljs/global');
-
 const program = require('commander');
+const _ = require('lodash');
 
+program
+    .command('evsw')
+    .description('List environment variables that start with a given character')
+    .option('-x, --xchar', 'Character used to search env vars')
+    .action((xchar) => {
+        _
+            .chain(process.env)
+            .pickBy((value, key) => _.startsWith(key, xchar))
+            .forOwn((value, key) => console.log(`${key}=${value}`))
+            .value();
+    })
+    .on('--help', () => {
+        console.log('  Examples:');
+        console.log('    $ node index evsw -x N');
+    });
+
+program.parse(process.argv);
